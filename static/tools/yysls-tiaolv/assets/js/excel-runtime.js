86a4e2356438b08d18945701cf401da8f44dd2ad
ShangGuanPeiYin <xingshenxu@outlook.com>
2026-08-10 16:27:36 +0800

    调整调率站赛季抗性为2.45

--- static/tools/yysls-tiaolv/assets/js/excel-runtime.js
+++ static/tools/yysls-tiaolv/assets/js/excel-runtime.js
@@ -442,7 +442,7 @@
 
     function seasonResistance() {
         const seasonStats = window.CommonData && window.CommonData.SEASON_STATS || {};
-        return num(seasonStats["赛季抗性"]) || 1.45;
+        return num(seasonStats["赛季抗性"]) || 2.45;
     }
 
     function diyAssumedOuterPen() {
